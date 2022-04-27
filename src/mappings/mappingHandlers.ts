c2ef9dbe241079ee1105b85c6be7004e3968e739
shenger <714275326@qq.com>
2022-04-27 14:23:06 +0800

    first

---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,17 @@
+import { SubstrateBlock } from '@subql/types'
+import { Block } from '../types'
+
+export async function handleBlock(block: SubstrateBlock): Promise<void> {
+  const number = block.block.header.number.toString();
+  const extrinsicForSetTimestamp = block.block.extrinsics.find((item) => {
+    return item.method.method === 'set'
+      && item.method.section === 'timestamp'
+  })
+  
+  const timestamp = extrinsicForSetTimestamp ? new Date(Number(extrinsicForSetTimestamp?.args?.[0].toString())) : new Date();
+  const record = new Block(number);
+  record.number = BigInt(number);
+  record.timestamp = timestamp;
+
+  await record.save();
+}
